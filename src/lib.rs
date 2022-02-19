a2c2c1bc453b299c45946dffcaad0eb53fece02b
Benjamin Saunders <ben.e.saunders@gmail.com>
2022-02-19 13:25:12 -0800

    Add derivative-preserving arithmetic on Sample

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![feature(portable_simd)]
 
-use std::simd::{LaneCount, Mask, Simd, StdFloat as _, SupportedLaneCount};
+use std::{
+    ops::{Add, AddAssign, Mul, MulAssign},
+    simd::{LaneCount, Mask, Simd, StdFloat as _, SupportedLaneCount},
+};
 
 #[cfg(feature = "rand")]
 use rand::{
@@ -356,6 +359,53 @@ where
 {
     pub value: Simd<f32, LANES>,
     pub derivative: [Simd<f32, LANES>; DIMENSION],
+}
+
+impl<const LANES: usize, const DIMENSION: usize> AddAssign<Self> for Sample<LANES, DIMENSION>
+where
+    LaneCount<LANES>: SupportedLaneCount,
+{
+    fn add_assign(&mut self, other: Self) {
+        self.value += other.value;
+        for i in 0..DIMENSION {
+            self.derivative[i] += other.derivative[i];
+        }
+    }
+}
+
+impl<const LANES: usize, const DIMENSION: usize> Add<Self> for Sample<LANES, DIMENSION>
+where
+    LaneCount<LANES>: SupportedLaneCount,
+{
+    type Output = Self;
+    fn add(mut self, other: Self) -> Self {
+        self += other;
+        self
+    }
+}
+
+impl<const LANES: usize, const DIMENSION: usize> MulAssign<Simd<f32, LANES>>
+    for Sample<LANES, DIMENSION>
+where
+    LaneCount<LANES>: SupportedLaneCount,
+{
+    fn mul_assign(&mut self, other: Simd<f32, LANES>) {
+        self.value *= other;
+        for i in 0..DIMENSION {
+            self.derivative[i] *= other;
+        }
+    }
+}
+
+impl<const LANES: usize, const DIMENSION: usize> Mul<Simd<f32, LANES>> for Sample<LANES, DIMENSION>
+where
+    LaneCount<LANES>: SupportedLaneCount,
+{
+    type Output = Self;
+    fn mul(mut self, other: Simd<f32, LANES>) -> Self {
+        self *= other;
+        self
+    }
 }
 
 const DEFAULT_PERM: [i32; 512] = [
