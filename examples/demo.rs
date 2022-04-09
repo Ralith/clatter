cbc2a2549069fd873405b2e839cf88f65ebcfcff
Benjamin Saunders <ben.e.saunders@gmail.com>
2022-04-09 10:03:23 -0700

    FBM demo

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,17 +1,33 @@
 #![feature(portable_simd)]
 
-use std::{simd::Simd, time::Instant};
+use std::{
+    simd::{LaneCount, Simd, SupportedLaneCount},
+    time::Instant,
+};
 
 use clap::Parser;
 
-use clatter::Simplex4d;
+use clatter::{Sample, Simplex2d};
 
+/// Compute a patch of fractal brownian motion noise
 #[derive(Parser)]
+#[clap(name = "demo")]
 struct Opts {
+    /// Length of the sampled area's edges
     #[clap(short, default_value = "5.0")]
     scale: f32,
+    /// Resolution of the output image
     #[clap(short, default_value = "512")]
     resolution: usize,
+    /// Number of times to sample the noise
+    #[clap(short, default_value = "4")]
+    octaves: usize,
+    /// How smooth the noise should be (sensible values are around 0.5-1)
+    #[clap(short, default_value = "1.0")]
+    hurst_exponent: f32,
+    /// Frequency ratio between successive octaves
+    #[clap(short, default_value = "1.9")]
+    lacunarity: f32,
 }
 
 const N: usize = 8;
@@ -19,7 +35,6 @@ const N: usize = 8;
 fn main() {
     let opts = Opts::parse();
     let step = opts.scale / opts.resolution as f32;
-    let noise = Simplex4d::new();
     let mut pixels = Vec::with_capacity(opts.resolution * opts.resolution);
 
     let start = Instant::now();
@@ -31,10 +46,13 @@ fn main() {
                 px[i] += step * i as f32;
             }
             let py = Simd::splat(y as f32 * step);
-            let pz = Simd::splat(0.5);
-            let pw = Simd::splat(0.7);
 
-            let sample = noise.sample::<N>([px, py, pz, pw]);
+            let sample = fbm::<N>(
+                opts.octaves,
+                (-opts.hurst_exponent).exp2(),
+                opts.lacunarity,
+                [px, py],
+            );
             let value = (Simd::splat(255.0) * (sample.value + Simd::splat(1.0)) / Simd::splat(2.0))
                 .cast::<u8>();
             for i in 0..N {
@@ -63,4 +81,29 @@ fn main() {
         8,
     )
     .unwrap();
+}
+
+fn fbm<const LANES: usize>(
+    octaves: usize,
+    gain: f32,
+    lacunarity: f32,
+    point: [Simd<f32, LANES>; 2],
+) -> Sample<LANES, 2>
+where
+    LaneCount<LANES>: SupportedLaneCount,
+{
+    const NOISE: Simplex2d = Simplex2d::new();
+
+    let mut result = Sample::default();
+
+    let mut frequency = 1.0;
+    let mut amplitude = 1.0;
+    let mut scale = 0.0;
+    for _ in 0..octaves {
+        result += NOISE.sample(point.map(|x| x * Simd::splat(frequency))) * Simd::splat(amplitude);
+        scale += amplitude;
+        frequency *= lacunarity;
+        amplitude *= gain;
+    }
+    result / Simd::splat(scale)
 }
--- src/sample.rs
+++ src/sample.rs
@@ -1,5 +1,5 @@
 use std::{
-    ops::{Add, AddAssign, Mul, MulAssign},
+    ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign},
     simd::{LaneCount, Simd, SupportedLaneCount},
 };
 
@@ -10,6 +10,27 @@ where
 {
     pub value: Simd<f32, LANES>,
     pub derivative: [Simd<f32, LANES>; DIMENSION],
+}
+
+impl<const LANES: usize, const DIMENSION: usize> Sample<LANES, DIMENSION>
+where
+    LaneCount<LANES>: SupportedLaneCount,
+{
+    pub const fn constant(value: Simd<f32, LANES>) -> Self {
+        Self {
+            value,
+            derivative: [Simd::splat(0.0); DIMENSION],
+        }
+    }
+}
+
+impl<const LANES: usize, const DIMENSION: usize> Default for Sample<LANES, DIMENSION>
+where
+    LaneCount<LANES>: SupportedLaneCount,
+{
+    fn default() -> Self {
+        Self::constant(Simd::splat(0.0))
+    }
 }
 
 impl<const LANES: usize, const DIMENSION: usize> AddAssign<Self> for Sample<LANES, DIMENSION>
@@ -55,6 +76,30 @@ where
     type Output = Self;
     fn mul(mut self, other: Simd<f32, LANES>) -> Self {
         self *= other;
+        self
+    }
+}
+
+impl<const LANES: usize, const DIMENSION: usize> DivAssign<Simd<f32, LANES>>
+    for Sample<LANES, DIMENSION>
+where
+    LaneCount<LANES>: SupportedLaneCount,
+{
+    fn div_assign(&mut self, other: Simd<f32, LANES>) {
+        self.value /= other;
+        for i in 0..DIMENSION {
+            self.derivative[i] /= other;
+        }
+    }
+}
+
+impl<const LANES: usize, const DIMENSION: usize> Div<Simd<f32, LANES>> for Sample<LANES, DIMENSION>
+where
+    LaneCount<LANES>: SupportedLaneCount,
+{
+    type Output = Self;
+    fn div(mut self, other: Simd<f32, LANES>) -> Self {
+        self /= other;
         self
     }
 }
